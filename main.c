c725d024f43a680576cefea1b62457b02bac1f7f
annakllara <annakllara35@gmail.com>
2025-09-04 00:53:45 +0000

    correção de formatação

--- main.c
+++ main.c
@@ -11,11 +11,7 @@ int main() {
     for (i = 0; i < 10; i++) {
         printf("Valor %d: ", i + 1);
         scanf("%d", &vetor[i]);
-    }
-
-    // Assume que o primeiro elemento é o maior inicialmente
-    maiorValor = vetor[0];
-
+        
     // Loop para encontrar o maior valor no vetor
     for (i = 1; i < 10; i++) {
         if (vetor[i] > maiorValor) {
